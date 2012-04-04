1e4c53c0aa51851b5e2a4627df8e6ae6cf83011c
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2012-04-05 00:37:58 +0900

    use TOP_DIR/dump/ instead of TOP_DIR/{mysqldump,pg_dump}/

--- init.sh
+++ init.sh
@@ -7,12 +7,11 @@ cd "$TOP_DIR"
 TOP_DIR="$(pwd)"
 chown root:adm "$TOP_DIR"
 chmod 2750 "$TOP_DIR"
-chown root:adm *.sh local-dump
-chmod 755 *.sh local-dump
-cp local-dump /etc/cron.daily/local-dump
+chown root:adm .gitignore README* *.sh local-dump.in
+chmod 755 *.sh local-dump.in
+sed -e "s:TOP_DIR:$TOP_DIR:" local-dump.in > /etc/cron.daily/local-dump
 chmod 755 /etc/cron.daily/local-dump
-mkdir -p mysqldump pg_dump
-
+mkdir -p dump
 if [ ! -f /etc/tmpreaper.conf ]; then
     apt-get install tmpreaper
 fi
@@ -20,9 +19,6 @@ if [ ! -f /etc/tmpreaper.conf ]; then
     echo "/etc/tmpreaper.conf not found" 1>&2
     exit 1
 fi
-if ! egrep -q "^TMPREAPER_DIRS=.*$TOP_DIR/mysqldump/" /etc/tmpreaper.conf >/dev/null 2>&1; then
-    echo 'TMPREAPER_DIRS="$TMPREAPER_DIRS '"$TOP_DIR"'/mysqldump/."' >> /etc/tmpreaper.conf
-fi
-if ! egrep -q "^TMPREAPER_DIRS=.*$TOP_DIR/pg_dump/" /etc/tmpreaper.conf >/dev/null 2>&1; then
-    echo 'TMPREAPER_DIRS="$TMPREAPER_DIRS '"$TOP_DIR"'/pg_dump/."' >> /etc/tmpreaper.conf
+if ! egrep -q "^TMPREAPER_DIRS=.*$TOP_DIR/dump/" /etc/tmpreaper.conf >/dev/null 2>&1; then
+    echo 'TMPREAPER_DIRS="$TMPREAPER_DIRS '"$TOP_DIR"'/dump/."' >> /etc/tmpreaper.conf
 fi
